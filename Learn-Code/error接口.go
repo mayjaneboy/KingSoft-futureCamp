e976b8d7e79d10c910bb7537673ba2cec8e3933a
mayjaneboy <mayjaneboy@qq.com>
2025-03-28 20:10:48 +0800

    some

---
+++ Learn-Code/error接口.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+// 自定义错误类型
+type MyError struct {
+	Code    int
+	Message string
+}
+
+// 实现 error 接口
+func (e *MyError) Error() string {
+	return fmt.Sprintf("错误码：%d，错误信息：%s", e.Code, e.Message)
+}
+
+// 使用自定义错误
+func doSomething() error {
+	return &MyError{
+		Code:    404,
+		Message: "资源未找到",
+	}
+}
+
+func main() {
+	err := errors.New("这是一个错误")
+	fmt.Println(err)
+
+	err2 := fmt.Errorf("错误信息: %w", err)
+	fmt.Println(err2)
+
+}
