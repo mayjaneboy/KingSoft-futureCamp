cfe4d51826b1e765b12eca33978d69221b2b4f17
mayjaneboy <mayjaneboy@qq.com>
2025-05-09 16:05:06 +0800

    feat:add some update

--- test.go
+++ test.go
@@ -1,19 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func GetValue() int {
-	return 1
+// 模拟从文件读取数据的函数
+func readFile(fileName string, resultChan chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 这里简单模拟读取文件，实际应用中应使用文件读取操作
+	var data string
+	switch fileName {
+	case "file1.txt":
+		data = "Content of file1"
+	case "file2.txt":
+		data = "Content of file2"
+	case "file3.txt":
+		data = "Content of file3"
+	}
+	resultChan <- fmt.Sprintf("Read data from %s: %s", fileName, data)
 }
 
 func main() {
-	s1 := []int{3, 4, 5, 6}
-	fmt.Println("len s1", len(s1))
-	fmt.Println("cap s1", cap(s1))
+	fileNames := []string{"file1.txt", "file2.txt", "file3.txt"}
+	var wg sync.WaitGroup
+	resultChan := make(chan string)
 
-	s2 := s1[1:3]
-	fmt.Println("len s2", len(s2))
-	fmt.Println("cap s2", cap(s2))
+	for _, fileName := range fileNames {
+		wg.Add(1)
+		go readFile(fileName, resultChan, &wg)
+	}
 
-	fmt.Println("s2", s2)
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for result := range resultChan {
+		fmt.Println(result)
+	}
+
+	fmt.Println("All files have been read.")
 }
